652141133781a766269049b5e6160a03b3c7140e
Karim M. Abdellatif <karim.m.abdellatif@gmail.com>
2024-05-20 20:22:50 +0200

    fixing cleaning

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version = "0.1",
     description  = "A tool for state of the artr DL attacks",
     author = "Karim M. Abdellatif",
+    packages = ["scadl"],
     install_requires = [
         "numpy",
         "keras",
