fixing normalization range

scadl/tools.py
@@ -539,18 +539,19 @@ inv_sbox = [
 ]
 
 
-def normalization(data):
+def normalization(data: np.ndarray, feature_range=(0, 1)):
     """It accepts data as a np array and returns normlized data between 0 and 1"""
-    return (data - np.min(data, axis=0)) / (np.max(data, axis=0) - np.min(data, axis=0))
+    x_std = (data - np.min(data, axis=0)) / (np.max(data, axis=0) - np.min(data, axis=0))
+    return x_std * (max(feature_range) - min(feature_range)) + min(feature_range)
 
 
-def remove_avg(traces):
+def remove_avg(traces: np.ndarray):
     """It takes traces as a np array and returns the subtracted average traces"""
     avg = np.average(traces, axis=0)
     return traces - avg
 
 
-def gen_labels(leakage_model, metadata: np.array, key_byte):
+def gen_labels(leakage_model, metadata: np.ndarray, key_byte: int):
     """It is used to generate labels from metadata
     It takes leakage_model as a leakage function,
     metadata,

tutorial/non_profile/ascad/non_profile_ascad.py
@@ -45,7 +45,7 @@ def mlp_short(len_samples):
     model.add(Dense(50, input_dim=len_samples, activation="relu"))
     model.add(Dense(20, activation="relu"))
     model.add(Dense(2, activation="softmax"))
-    optimizer = "adam"  # RMSprop(lr=0.00001)
+    optimizer = "adam" 
     model.compile(
         loss="categorical_crossentropy",
         optimizer=optimizer,
@@ -54,19 +54,6 @@ def mlp_short(len_samples):
     return model
 
 
-def mlp_ascad(node=600, layer_nb=6):
-    model = Sequential()
-    model.add(Dense(node, input_dim=700, activation="relu"))
-    for i in range(layer_nb - 2):
-        model.add(Dense(node, activation="relu"))
-        # Dropout(0.1)    #Dropout(0.01)
-        # BatchNormalization()
-    model.add(Dense(2, activation="softmax"))
-    optimizer = "adam"  # RMSprop(lr=0.00001)
-    model.compile(
-        loss="categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"]
-    )
-    return model
 
 
 if __name__ == "__main__":
@@ -82,13 +69,9 @@ if __name__ == "__main__":
     correct_key = metadata["key"][0][TARGET_BYTE]
 
     """Subtracting average from traces + normalization"""
-    poi = np.concatenate((leakages[:, 515:520], leakages[:, 148:158]), axis=1)
-    avg = remove_avg(leakages)
-    x_train = avg
-
+    x_train = normalization(remove_avg(leakages), feature_range=(-1, 1))
     """Selecting the model"""
     model_dl = mlp_short(x_train.shape[1])
-    # model_dl = mlp_non_profiling(x_train.shape)
     # model = cnn_best(x_train.shape[1], key_range)
 
     """Non-profiling DL"""
@@ -97,9 +80,9 @@ if __name__ == "__main__":
         x_train=x_train,
         metadata=metadata,
         hist_acc="val_accuracy",
-        key_range=range(0, 256),
+        key_range=range(50, 58),
         num_classes=2,
-        epochs=100,
+        epochs=50,
         batch_size=1000,
     )
 
@@ -107,7 +90,7 @@ if __name__ == "__main__":
     guessed_key = np.argmax(np.max(acc, axis=1))
     print(f"guessed key = {guessed_key}")
     plt.plot(acc.T, "grey", linewidth=2)
-    plt.plot(acc[4], "black", linewidth=2)
+    plt.plot(acc[correct_key], "black", linewidth=2)
     plt.xlabel("Number of epochs", fontsize=40)
     plt.ylabel("Accuracy ", fontsize=40)
     plt.xticks(fontsize=25)

tutorial/profile/ascad/profile.py
@@ -38,10 +38,9 @@ def aug_crop(x, y):
 def mlp_short(len_samples):
     """It returns an MLP model"""
     model = Sequential()
-    model.add(Dense(50, input_dim=len_samples, activation="relu"))
-    model.add(Dense(20, activation="relu"))
-    # model.add(Dense(300, activation="relu"))
-    # model.add(Dense(300, activation="relu"))
+    model.add(Dense(20, input_dim=len_samples, activation="relu"))
+    # BatchNormalization()
+    model.add(Dense(50, activation="relu"))
     model.add(Dense(256, activation="softmax"))
     optimizer = "adam"  # RMSprop(learning_rate=0.00001) #tf.keras.optimizers.Adam() # RMSprop(lr=0.00001)
     model.compile(
@@ -64,11 +63,12 @@ if __name__ == "__main__":
     metadata = file["Profiling_traces"]["metadata"][:]
 
     """Selecting poi where SNR gives the max value"""
-    poi = np.concatenate((leakages[:, 515:520], leakages[:, 148:158]), axis=1)
+    poi = leakages # np.concatenate((leakages[:, 515:520], leakages[:, 148:158]), axis=1)
     # poi = leakages - np.mean(leakages, axis=0)
 
     """Processing the traces"""
-    x_train = normalization(remove_avg(poi))
+    x_train = normalization(remove_avg(poi), feature_range=(-1, 1))
+    # x_train = handy_normalization(remove_avg(poi))
     GUESS_RANGE = 256
 
     """Loading the DL model mlp"""
@@ -83,7 +83,7 @@ if __name__ == "__main__":
         metadata=metadata,
         epochs=50,
         batch_size=128,
-        validation_split=0.02,
+        validation_split=0.1,
         data_augmentation=False,
     )
     profile_engine.save_model("model.keras")

tutorial/profile/ascad/test.py
@@ -12,6 +12,7 @@ def leakage_model(data, guess):
     return sbox[guess ^ data["plaintext"][2]]
 
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Need to specify the location of training data")
@@ -28,8 +29,8 @@ if __name__ == "__main__":
 
     """Selecting poi where SNR gives the max value and it should have 
     the same index like what is used in the profiling phase """
-    poi = np.concatenate((leakages[:, 515:520], leakages[:, 148:158]), axis=1)
-    poi = normalization(remove_avg(poi))
+    poi = leakages # np.concatenate((leakages[:, 515:520], leakages[:, 148:158]), axis=1)
+    poi = normalization(remove_avg(poi), feature_range=(-1, 1))
 
     # Normalization is used for improving the learning
 