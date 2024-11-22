5647c162f310310ef60fa8efac69ef066ddd10d3
TrAyZeN <leo.benito@ledger.fr>
2024-11-22 11:28:18 +0100

    Add check formatting in CI

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="scadl",
     version="0.1",
-    description="A tool for state of the artr DL attacks",
+    description="A tool for state of the art deep learning based side-channel attacks",
     author="Karim M. Abdellatif",
     packages=find_packages(),
     install_requires=["numpy", "keras", "matplotlib", "tqdm", "h5py"],
